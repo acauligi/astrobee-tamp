487fff9525badde2968cd342155156d74ed9f50b
Abhishek Cauligi <acauligi@stanford.edu>
2020-08-27 15:12:33 -0700

    Changed around order of adding vars

--- astrobee_tamp.py
+++ astrobee_tamp.py
@@ -35,10 +35,10 @@ class AstrobeeTAMP:
         self.R = np.eye(self.m)
 
         # environment parameters
-        self.dock_locations = {}
-        self.dock_locations['A'] = np.array([10.,20.])
-        self.dock_locations['B'] = np.array([10.,30.])
-        self.dock_locations['C'] = np.array([10.,40.])
+        self.dock_loc = {}
+        self.dock_loc['A'] = np.array([10., 15.])
+        self.dock_loc['B'] = np.array([20., 15.])
+        self.dock_loc['C'] = np.array([30., 15.])
 
         # robot parameters
         J = np.array([[0.1083, 0.0, 0.0],
@@ -52,6 +52,8 @@ class AstrobeeTAMP:
         hard_limit_accel = 1000. # 0.10
         hard_limit_omega = 45*np.pi/180
         hard_limit_alpha = 50*np.pi/180
+
+        self.arm_length = 0.1
 
         self.params = {}
         self.params['mass'] = mass
@@ -130,11 +132,42 @@ class AstrobeeTAMP:
     def unroll_plan(self): 
         self.cost = 0.
         self.X, self.U = [], []
+        self.Xg = []
         self.w, self.lbw, self.ubw = [], [], []
         self.g, self.lbg, self.ubg = [], [], []
 
+        # Create decision variables for free-flyer
+        # for operator in self.plan:
+            # w_loc, lbw_loc, ubw_loc, g_loc, lbg_loc, ubg_loc, X_loc, U_loc \
+            #     = self.generate_segment_vars()
+            # self.w += [*w_loc]
+            # self.lbw += [*lbw_loc]
+            # self.ubw += [*ubw_loc]
+            # self.g += [*g_loc]
+            # self.lbg += [*lbg_loc]
+            # self.ubg += [*ubg_loc]
+
         for operator in self.plan:
-            w_loc, lbw_loc, ubw_loc, g_loc, lbg_loc, ubg_loc, X_loc, U_loc, J_loc = self.operator_cons(operator)
+            X, lbx, ubx = self.create_vars(self.N, self.Xlb, self.Xub, tag='X_')
+            self.w += [*X]
+            self.lbw += [*lbx]
+            self.ubw += [*ubx]
+            self.X.extend(X)
+
+        for operator in self.plan:
+            U, lbu, ubu = self.create_vars(self.N, self.Ulb, self.Uub, tag='U_')
+            self.w += [*U]
+            self.lbw += [*lbu]
+            self.ubw += [*ubu]
+            self.U.extend(U)
+
+        # Create additional vars and constraints for grasping
+        for ii, operator in enumerate(self.plan):
+            X_loc, U_loc = self.X[self.N*ii:self.N*(ii+1)], self.U[self.N*ii:self.N*(ii+1)] 
+
+            w_loc, lbw_loc, ubw_loc, \
+              g_loc, lbg_loc, ubg_loc, \
+              Xg_loc, J_loc = self.operator_cons(operator, X_loc, U_loc)
             if len(w_loc) == 0:
                 continue
             self.cost += (J_loc + self.lqr_cost(X_loc, U_loc, self.Xref))
@@ -144,8 +177,9 @@ class AstrobeeTAMP:
             self.g += [*g_loc]
             self.lbg += [*lbg_loc]
             self.ubg += [*ubg_loc]
-            self.X.extend(X_loc)
-            self.U.extend(U_loc)
+
+            if Xg_loc is not None:
+                self.Xg.extend(Xg_loc)
 
         # Add dynamics constraints
         self.init_straightline()
@@ -180,35 +214,35 @@ class AstrobeeTAMP:
 
         return True if self.solver.stats()['success'] else False
 
-    def operator_cons(self, operator):
+    def operator_cons(self, operator, X_loc, U_loc):
         if operator == 'dock_objA_dockA':
-            return self.dock_objA_dockA()
+            return self.dock_objA_dockA(X_loc, U_loc)
         elif operator == 'dock_objA_dockB':
-            return self.dock_objA_dockB()
+            return self.dock_objA_dockB(X_loc, U_loc)
         elif operator == 'dock_objA_dockC':
-            return self.dock_objA_dockC()
+            return self.dock_objA_dockC(X_loc, U_loc)
         elif operator == 'dock_objB_dockA':
-            return self.dock_objB_dockA()
+            return self.dock_objB_dockA(X_loc, U_loc)
         elif operator == 'dock_objB_dockB':
-            return self.dock_objB_dockB()
+            return self.dock_objB_dockB(X_loc, U_loc)
         elif operator == 'dock_objB_dockC':
-            return self.dock_objB_dockC()
+            return self.dock_objB_dockC(X_loc, U_loc)
         elif operator == 'undock_objA_dockA':
-            return self.undock_objA_dockA()
+            return self.undock_objA_dockA(X_loc, U_loc)
         elif operator == 'undock_objA_dockB':
-            return self.undock_objA_dockB()
+            return self.undock_objA_dockB(X_loc, U_loc)
         elif operator == 'undock_objA_dockC':
-            return self.undock_objA_dockC()
+            return self.undock_objA_dockC(X_loc, U_loc)
         elif operator == 'undock_objB_dockA':
-            return self.undock_objB_dockA()
+            return self.undock_objB_dockA(X_loc, U_loc)
         elif operator == 'undock_objB_dockB':
-            return self.undock_objB_dockB()
+            return self.undock_objB_dockB(X_loc, U_loc)
         elif operator == 'undock_objB_dockC':
-            return self.undock_objB_dockC()
+            return self.undock_objB_dockC(X_loc, U_loc)
         elif operator == 'grasp_objA':
-            return self.grasp_objA()
+            return self.grasp_objA(X_loc, U_loc)
         elif operator == 'grasp_objB':
-            return self.grasp_objB()
+            return self.grasp_objB(X_loc, U_loc)
 
     def create_vars(self, N, lb, ub, tag='X_'):
         n = lb.size 
@@ -272,92 +306,276 @@ class AstrobeeTAMP:
 
         return w, lbw, ubw, g, lbg, ubg
 
-    def add_grasp_con(self, obj_id):
+    def add_grasp_con(self, X, obj_X):
         # TODO(acauligi): compute forward kinematics given
         # reference trajectory and add equality constraint
         w, lbw, ubw = [], [], []
         g, lbg, ubg = [], [], []
 
-        for ii in range(N):
-            g += []
-            lbg += []
-            ubg += []
+        for ii in range(len(X)):
+            g += [X[ii][0] - obj_X[ii][0]]
+            lbg += [0.]
+            ubg += [0.]
+
+            g += [X[ii][1]+self.arm_length - obj_X[ii][1]]
+            lbg += [0.]
+            ubg += [0.]
         return w, lbw, ubw, g, lbg, ubg, 0.
 
-    def add_dock_con(self, obj_id, dock_coord):
+    def add_dock_con(self, obj_X, dock_loc):
         w, lbw, ubw = [], [], []
         g, lbg, ubg = [], [], []
 
-        # TODO(acauligi): add constraint to end of trajectory
+        for ii in range(2):
+            g += [obj_X[-1][ii] - dock_loc[ii]]
+            lbg += [0.]
+            ubg += [0.]
 
         return w, lbw, ubw, g, lbg, ubg
 
-    def dock_objA_dockA(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+    def add_grasp_dock_con(self, X, dock_id):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
 
-    def dock_objA_dockB(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        obj_X = None
+        tag = 'obj{}_'.format(dock_id)
 
-    def dock_objA_dockC(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        obj_X, lb_obj, ub_obj = self.create_vars(self.N, self.Xlb, self.Xub, tag=tag)
+        w.extend(obj_X)
+        lbw.extend(lb_obj)
+        ubw.extend(ub_obj)
 
-    def dock_objB_dockA(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        w_grasp, lbw_grasp, ubw_grasp, g_grasp, lbg_grasp, ubg_grasp, _  = self.add_grasp_con(X, obj_X)
+        g.extend(g_grasp)
+        lbg.extend(lbg_grasp)
+        ubg.extend(ubg_grasp)
 
-    def dock_objB_dockB(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        w_dock, lbw_dock, ubw_dock, g_dock, lbg_dock, ubg_dock = self.add_dock_con(obj_X, self.dock_loc['A'])
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+        return obj_X, w, lbw, ubw, g, lbg, ubg
 
-    def dock_objB_dockC(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
+    def dock_objA_dockA(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
         J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
 
-    def undock_objA_dockA(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        dock_id = 'A'
+        X_objA, w_objA, lbw_objA, ubw_objA, g_objA, lbg_objA, ubg_objA= self.add_grasp_dock_con(X, dock_id)
+        w.extend(w_objA)
+        lbw.extend(lbw_objA)
+        ubw.extend(ubw_objA)
+        g.extend(g_objA)
+        lbg.extend(lbg_objA)
+        ubg.extend(ubg_objA)
 
-    def undock_objA_dockB(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
 
-    def undock_objA_dockC(self):
-        w, lbw, ubw, g, lbg, ubg, J = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+    def dock_objA_dockB(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
 
-    def undock_objB_dockA(self):
-        w, lbw, ubw, g, lbg, ubg, J = self.generate_segment_vars()
+        X_objA = None
         J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
 
-    def undock_objB_dockB(self):
-        w, lbw, ubw, g, lbg, ubg, J = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        dock_id = 'B'
+        X_objA, w_objA, lbw_objA, ubw_objA, g_objA, lbg_objA, ubg_objA= self.add_grasp_dock_con(X, dock_id)
+        w.extend(w_objA)
+        lbw.extend(lbw_objA)
+        ubw.extend(ubw_objA)
+        g.extend(g_objA)
+        lbg.extend(lbg_objA)
+        ubg.extend(ubg_objA)
 
-    def undock_objB_dockC(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
 
-    def grasp_objA(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
-        J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+    def dock_objA_dockC(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
 
-    def grasp_objB(self):
-        w, lbw, ubw, g, lbg, ubg, X, U = self.generate_segment_vars()
+        X_objA = None
         J = 0.
-        return w, lbw, ubw, g, lbg, ubg, X, U, J
+
+        dock_id = 'C'
+        X_objA, w_objA, lbw_objA, ubw_objA, g_objA, lbg_objA, ubg_objA= self.add_grasp_dock_con(X, dock_id)
+        w.extend(w_objA)
+        lbw.extend(lbw_objA)
+        ubw.extend(ubw_objA)
+        g.extend(g_objA)
+        lbg.extend(lbg_objA)
+        ubg.extend(ubg_objA)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
+
+    def dock_objB_dockA(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+
+        dock_id = 'A'
+        X_objB, w_objB, lbw_objB, ubw_objB, g_objB, lbg_objB, ubg_objB= self.add_grasp_dock_con(X, dock_id)
+        w.extend(w_objB)
+        lbw.extend(lbw_objB)
+        ubw.extend(ubw_objB)
+        g.extend(g_objB)
+        lbg.extend(lbg_objB)
+        ubg.extend(ubg_objB)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
+
+    def dock_objB_dockB(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+
+        dock_id = 'B'
+        X_objB, w_objB, lbw_objB, ubw_objB, g_objB, lbg_objB, ubg_objB= self.add_grasp_dock_con(X, dock_id)
+        w.extend(w_objB)
+        lbw.extend(lbw_objB)
+        ubw.extend(ubw_objB)
+        g.extend(g_objB)
+        lbg.extend(lbg_objB)
+        ubg.extend(ubg_objB)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
+
+    def dock_objB_dockC(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+
+        dock_id = 'C'
+        X_objB, w_objB, lbw_objB, ubw_objB, g_objB, lbg_objB, ubg_objB= self.add_grasp_dock_con(X, dock_id)
+        w.extend(w_objB)
+        lbw.extend(lbw_objB)
+        ubw.extend(ubw_objB)
+        g.extend(g_objB)
+        lbg.extend(lbg_objB)
+        ubg.extend(ubg_objB)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
+
+    def undock_objA_dockA(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objA = None
+        J = 0.
+
+        dock_id = 'A'
+        obj_loc = self.dock_loc[dock_id]
+        obj_loc[1] -= self.arm_length
+        _, _, _, g_dock, lbg_dock, ubg_dock = self.add_dock_con(X, obj_loc)
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
+
+    def undock_objA_dockB(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objA = None
+        J = 0.
+
+        dock_id = 'B'
+        obj_loc = self.dock_loc[dock_id]
+        obj_loc[1] -= self.arm_length
+        _, _, _, g_dock, lbg_dock, ubg_dock = self.add_dock_con(X, obj_loc)
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
+
+    def undock_objA_dockC(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objA = None
+        J = 0.
+
+        dock_id = 'C'
+        obj_loc = self.dock_loc[dock_id]
+        obj_loc[1] -= self.arm_length
+        _, _, _, g_dock, lbg_dock, ubg_dock = self.add_dock_con(X, obj_loc)
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
+
+    def undock_objB_dockA(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+
+        dock_id = 'A'
+        obj_loc = self.dock_loc[dock_id]
+        obj_loc[1] -= self.arm_length
+        _, _, _, g_dock, lbg_dock, ubg_dock = self.add_dock_con(X, obj_loc)
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
+
+    def undock_objB_dockB(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+
+        dock_id = 'B'
+        obj_loc = self.dock_loc[dock_id]
+        obj_loc[1] -= self.arm_length
+        _, _, _, g_dock, lbg_dock, ubg_dock = self.add_dock_con(X, obj_loc)
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
+
+    def undock_objB_dockC(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+
+        dock_id = 'C'
+        obj_loc = self.dock_loc[dock_id]
+        obj_loc[1] -= self.arm_length
+        _, _, _, g_dock, lbg_dock, ubg_dock = self.add_dock_con(X, obj_loc)
+        g.extend(g_dock)
+        lbg.extend(lbg_dock)
+        ubg.extend(ubg_dock)
+
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
+
+    def grasp_objA(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objA = None
+        J = 0.
+        return w, lbw, ubw, g, lbg, ubg, X_objA, J
+
+    def grasp_objB(self, X, U):
+        w, lbw, ubw = [], [], []
+        g, lbg, ubg = [], [], []
+
+        X_objB = None
+        J = 0.
+        return w, lbw, ubw, g, lbg, ubg, X_objB, J
